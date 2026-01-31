3b179430d7bea7bafaf5643f0e8d3e220a2dc8b2
Vessalia <nathanielples@outlook.com>
2026-01-30 23:28:44 -0600

    average profiler option

--- engine/internal/Fizziks/ScopeProfiler.h
+++ engine/internal/Fizziks/ScopeProfiler.h
@@ -2,6 +2,7 @@
 #include <chrono>
 #include <iostream>
 #include <string_view>
+#include <unordered_map>
 
 #if defined(_MSC_VER)
 #define PROFILE_FUNCTION_SIGNATURE __FUNCSIG__
@@ -16,25 +17,49 @@ class ScopeProfiler
 public:
     using clock = std::chrono::steady_clock;
 
-    ScopeProfiler(std::string_view label, const char* functionSignature)
+    ScopeProfiler(std::string_view label, const char* functionSignature, bool average = false)
         : label(label)
         , functionSignature(functionSignature)
-        , start(clock::now()) { }
+        , start(clock::now())
+        , average(average) { }
 
     ~ScopeProfiler()
     {
         const auto end = clock::now();
         const auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
 
-        std::cout << "[PROFILE] " << label
-            << " | " << functionSignature
-            << " | " << microseconds << " us\n";
+        if (average)
+        {
+            if (!averages.contains(label)) averages[label] = { microseconds, 1 };
+            else
+            {
+                averages[label].first += microseconds;
+                ++averages[label].second;
+            }
+
+            auto average = averages[label].first / averages[label].second;
+            print(average);
+        }
+        else
+        {
+            print(microseconds);
+        }
     }
 
     ScopeProfiler(const ScopeProfiler&) = delete;
     ScopeProfiler& operator=(const ScopeProfiler&) = delete;
 
+    void print(long long time) const
+    {
+        std::cout << "[PROFILE] " << label
+        << " | " << functionSignature
+        << " | " << time << " us\n";
+    }
+
 private:
+    static std::unordered_map<std::string_view, std::pair<long long, long long>> averages;
+
+    bool average;
     std::string_view label;
     const char* functionSignature;
     clock::time_point start;
@@ -46,5 +71,11 @@ private:
 #define PROFILE_SCOPE(label) \
     ScopeProfiler PROFILE_CONCAT(scopeProfiler, __LINE__)(label, PROFILE_FUNCTION_SIGNATURE)
 
+#define PROFILE_SCOPE_AVG(label) \
+    ScopeProfiler PROFILE_CONCAT(scopeProfiler, __LINE__)(label, PROFILE_FUNCTION_SIGNATURE, true)
+
 #define PROFILE_FUNCTION() \
     PROFILE_SCOPE(__func__)
+
+#define PROFILE_FUNCTION_AVG() \
+    PROFILE_SCOPE_AVG(__func__)
